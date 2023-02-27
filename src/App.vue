b21b14d279db361bee0bf2efefe0876d3b95be0c
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2023-02-27 18:55:29 +0100

    added simple todo app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,101 @@
-<script setup>
-
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="badass-todo">
+    <div class="title has-text-centered">Badass Todo</div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <form @submit.prevent="addTodo">
+      <div class="field is-grouped mb-5">
+        <p class="control is-expanded">
+          <input
+            v-model="newTodoContent"
+            class="input"
+            type="text"
+            placeholder="Add a todo"
+          />
+        </p>
+        <p class="control">
+          <button :disabled="!newTodoContent" class="button is-info">
+            Add
+          </button>
+        </p>
+      </div>
+    </form>
+
+    <div
+      v-for="todo in todos"
+      :class="{ 'has-background-success-light': todo.done }"
+      class="card mb-5"
+    >
+      <div class="card-content">
+        <div class="content">
+          <div class="columns is-mobile is-vcentered">
+            <div
+              :class="{ 'has-text-success line-through': todo.done }"
+              class="column"
+            >
+              {{ todo.content }}
+            </div>
+            <div class="column is-5 has-text-right">
+              <button
+                @click="toggleDone(todo.id)"
+                :class="todo.done ? 'is-success' : 'is-light'"
+                class="button"
+              >
+                &check;
+              </button>
+              <button
+                @click="deleteTodo(todo.id)"
+                class="button is-danger ml-2"
+              >
+                &cross;
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-
-  </main>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import { ref } from "vue";
+import { v4 as uuidv4 } from "uuid";
+
+const newTodoContent = ref("");
+
+const todos = ref([]);
+
+const addTodo = () => {
+  const newTodo = {
+    id: uuidv4(),
+    content: newTodoContent.value,
+    done: false,
+  };
+  todos.value.unshift(newTodo);
+  newTodoContent.value = "";
+};
+
+const deleteTodo = (id) => {
+  todos.value = todos.value.filter((todo) => {
+    return todo.id !== id;
+  });
+};
+
+const toggleDone = (id) => {
+  const index = todos.value.findIndex(todo => todo.id === id)
+  todos.value[index].done = !todos.value[index].done
+};
+</script>
+
+<style>
+@import "bulma\css\bulma.min.css";
+
+.badass-todo {
+  max-width: 400px;
+  padding: 20px;
+  margin: 0 auto;
+}
+
+.line-through {
+  text-decoration: line-through;
+}
+</style>
